45597084be02290a8465accee36a5c070a058e02
Mason <masonmccargish@gmail.com>
2025-02-02 00:04:32 -0700

    add gauge 2

---
+++ gauge.py
@@ -0,0 +1,22 @@
+from image import StressImage
+
+class StressGauge():
+    def __init__(self, x, y):
+        self.face = StressImage("gaugeface.png", x, y)
+        self.needle = StressImage("gaugeneedle.png", x, y)
+        self.front_face = StressImage("gaugefacefront.png", x, y)
+
+        self.needle_pos = 0
+
+        self.clickable = False
+    
+    def set_needle_pos(self, newpos):
+        self.needle_pos = newpos
+
+    def draw(self, DS, camera_offset_x=0):
+        self.face.draw(DS, camera_offset_x)
+        self.needle.draw(DS, camera_offset_x)
+        self.front_face.draw(DS, camera_offset_x)
+    
+    def checkclick(self):
+        return False
