e1cab4cd1973f4204199d72144a5429037101e65
mkr-mac <mccargishm@gmail.com>
2025-02-02 00:24:39 -0600

    Player

---
+++ player.py
@@ -0,0 +1,24 @@
+class Player():
+    def __init__(self):
+        self.stress = 0.0
+        self.job_security = 0
+        self.difficulty = 1
+
+    def update(self):
+        if stress > 1000:
+            self.die()
+
+        if self.stress > 0:
+            stress -= 1
+
+        if self.stress < 0:
+            self.stress = 0
+
+        self.difficulty += .0005
+
+
+    def increase_stress(self, active_stressors):
+        self.stress += active_stressors*self.difficulty
+
+    def die(self):
+        print (f"You ded. Your score:{(self.difficulty*1000)}")
