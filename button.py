20cfdd695551fd22b487b1f6b71f042dffafbbdd
Mason <masonmccargish@gmail.com>
2025-02-01 23:27:11 -0700

    a

--- button.py
+++ button.py
@@ -1,5 +1,5 @@
-import image
+from image import StressImage
 
 class StressButton(StressImage):
-    def __init__(self):
-        super(self)
+    def __init__(self, image, x, y, clickable = False, action = None):
+        super().__init__(image, x, y, clickable, action)
