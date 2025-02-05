738aa0fad238cb2cbdac980c6f59373153e16f5b
mkr-mac <mccargishm@gmail.com>
2025-02-04 23:11:17 -0600

    prepping stressobject

---
+++ stressobject.py
@@ -0,0 +1,15 @@
+class StressObject():
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def alarm(self):
+        pass
+
+    def clear_alarm(self):
+        pass
